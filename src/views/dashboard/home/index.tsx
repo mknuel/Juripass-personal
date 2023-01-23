a67376da51d7c68a8bdbd7d23cb13f8b7246bc0e
mknuel <your_email_address@example.com>
2023-01-23 17:52:59 +0100

    completed dashboard home

--- src/views/dashboard/home/index.tsx
+++ src/views/dashboard/home/index.tsx
@@ -3,16 +3,25 @@ import Head from "./subs/head";
 import Banner from "./subs/banner";
 import CasesSection from "./subs/cases";
 import style from "./home.module.scss";
+import Page from "../../../components/page";
+import FilesSection from "./subs/files";
 
 function Home() {
 	return (
-		<div className="dashboard">
-			<Head />
-			<Banner />
-			<div className={style.body}>
-				<CasesSection />
+		<Page title="Juripass | Dashboard">
+			<div className="dashboard">
+				<Head />
+				<Banner />
+				<div className={style.body}>
+					<div className={style["section"]}>
+						<CasesSection />
+					</div>
+					<div className={style["section"]}>
+						<FilesSection />
+					</div>
+				</div>
 			</div>
-		</div>
+		</Page>
 	);
 }
 
--- src/views/dashboard/home/subs/head/index.tsx
+++ src/views/dashboard/home/subs/head/index.tsx
@@ -1,7 +1,17 @@
-import { NotificationBellIcon } from "../../../../../assets/icons";
+import { useState } from "react";
+import {
+	CopyIcon,
+	MyFirmIcon,
+	NotificationBellIcon,
+	ProfileIcon,
+} from "../../../../../assets/icons";
 import { BtnPrimary, BtnTertiary, Btn } from "../../../../../components/button";
 import style from "../../home.module.scss";
 function Head() {
+	const [menu, showMenu] = useState<boolean>(false);
+	const toggleMenu = () => {
+		showMenu(!menu);
+	};
 	return (
 		<nav className={style.head}>
 			<div className={style.head__content}>
@@ -21,7 +31,38 @@ function Head() {
 					<NotificationBellIcon />
 				</Btn>
 
-				<Btn className={style.head__icon}>user deets</Btn>
+				<div className={style.head__icon}>
+					<Btn className={style.head__image} onClick={toggleMenu}>
+						<img src="./assets/juripassuser.png" alt="user" />
+					</Btn>
+					{menu && (
+						<div className={style.user}>
+							<div className={style.user__details}>
+								<img src="./assets/juripassuser.png" alt="" />
+
+								<h4> jeff Malone</h4>
+
+								<h6>
+									{" "}
+									<span>jeffmalone@gmail.com</span> &nbsp;
+									<CopyIcon />
+								</h6>
+							</div>
+							<div className={style.user__actions}>
+								<div>
+									<Btn className={style["user__btn"]}>
+										<MyFirmIcon /> <span>My Firm</span>
+									</Btn>
+									<Btn className={style["user__btn"]}>
+										<ProfileIcon /> <span>My Profile</span>
+									</Btn>
+								</div>
+
+								<BtnPrimary>Logout</BtnPrimary>
+							</div>
+						</div>
+					)}
+				</div>
 			</div>
 		</nav>
 	);
