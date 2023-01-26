4eafdbed80297b25c3180b6e313f42e516ab7274
Emmanuel Agboola <agboolaemmanuel524@gmail.com>
2023-01-26 13:06:20 +0100

    file change

--- src/views/landing/index.tsx
+++ src/views/landing/index.tsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import Footer from '../../components/footer'
-import Header from '../../components/header'
 import Page from '../../components/page'
-import Hero from '../../views/landing/hero'
-import Features from '../../views/landing/features'
-
-
-import './landing.scss'
-import About from './about'
-import Attestation from './attestation'
 
 function Landing() {
   return (
     <Page title='Juripass | Home'>
-      <div className='landing'>
-        <Hero />
-        <Features />
-        <About />
-        <Attestation />
+      <div>
+          This is the landing page
       </div>
     </Page>
   )
