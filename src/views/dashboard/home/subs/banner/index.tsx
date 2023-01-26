658a0ed44bf23786904a479fbe180c5162f57b6e
mknuel <your_email_address@example.com>
2023-01-26 11:28:54 +0100

    update

--- src/views/dashboard/home/subs/banner/index.tsx
+++ src/views/dashboard/home/subs/banner/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import style from "../../home.module.scss";
 import {
-	ArrowDown,
-	ArrowUp,
+	ArrowDownIcon,
+	ArrowUpIcon,
 	HammerIcon,
 } from "../../../../../components/icons/icons";
 
@@ -17,7 +17,7 @@ function Card() {
 
 			<div className={style["card__content--alt"]}>
 				<div className={`${style.card__status} ${style["card__status--red"]}`}>
-					<ArrowDown />
+					<ArrowDownIcon />
 					&nbsp;
 					<span className={"dark"}>25</span>
 					&nbsp;
@@ -26,7 +26,7 @@ function Card() {
 
 				<div
 					className={`${style.card__status} ${style["card__status--green"]}`}>
-					<ArrowUp />
+					<ArrowUpIcon />
 					&nbsp;
 					<span className={"dark"}>75</span>
 					&nbsp;
