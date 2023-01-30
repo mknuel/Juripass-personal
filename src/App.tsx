71eafe649c02b523b5795d0efe6884b8c2053cd6
miriam-samuels <medomiriam@gmail.com>
2023-01-30 16:23:06 +0100

    mobile res

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import DashboardLayout from './components/dashboard-layout';
 import Layout from './components/layout';
 import Loader from './components/loader';
 import * as ROUTE from './constants/routes'
-import './styles/index.css'
+import './styles/index.scss'
 
 // lazy load routes
 const SignUp = lazy(() => import('./views/signup'))
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Menu } from '@mui/icons-material'
 import { LOGO } from '../../assets/logos/logo'
 import * as ROUTE from '../../constants/routes'
 import { BtnPrimary, BtnSecondary, BtnTertiary } from '../button'
 import './index.scss';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   const active = {
     borderBottom: '3px solid #0D294D',
     fontWeight: 'bold'
@@ -23,12 +25,12 @@ function Header() {
             </NavLink>
           </div>
           <div>
-            <NavLink to={ROUTE.ABOUT}>
+            <NavLink to={ROUTE.ABOUT} style={({ isActive }) => isActive ? active : undefined}>
               About Us
             </NavLink>
           </div>
           <div>
-            <NavLink to={ROUTE.POLICY}>
+            <NavLink to={ROUTE.POLICY} style={({ isActive }) => isActive ? active : undefined}>
               Privacy Policy
             </NavLink>
           </div>
@@ -37,7 +39,37 @@ function Header() {
           <BtnSecondary>Login</BtnSecondary>
           <BtnPrimary>Sign Up</BtnPrimary>
         </div>
+
+        <Menu className='header-menu' onClick={() => setIsOpen(current => !current)} />
       </div>
+
+      {
+        isOpen &&
+        <div className='header-mobile'>
+          <nav className='header-mobile-nav'>
+            <div>
+              <NavLink to={ROUTE.LANDING} style={({ isActive }) => isActive ? active : undefined}>
+                Home
+              </NavLink>
+            </div>
+            <div>
+              <NavLink to={ROUTE.ABOUT} style={({ isActive }) => isActive ? active : undefined}>
+                About Us
+              </NavLink>
+            </div>
+            <div>
+              <NavLink to={ROUTE.POLICY} style={({ isActive }) => isActive ? active : undefined}>
+                Privacy Policy
+              </NavLink>
+            </div>
+          </nav>
+          <div className='header-mobile-btns'>
+            <BtnSecondary>Login</BtnSecondary>
+            <BtnPrimary>Sign Up</BtnPrimary>
+          </div>
+        </div>
+      }
+
     </header>
   )
 }
